5c2287ffc494649e8ff9e3a7fd19058f59d3a4c1
Alejandro Estrada <aestrada611@gmail.com>
2021-08-05 13:43:28 -0700

    adds new color scheme and better formatting

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {Box, Container, Grid, CardHeader, Slider} from '@material-ui/core'
+import {Slider, Card, AppBar, Toolbar, Typography, IconButton} from '@material-ui/core';
+import Draggable from 'react-draggable';
+import {Menu} from '@material-ui/icons';
+
 
 class HomeView extends React.Component{
 
-
+    
+    
     //think of better name for key but will handle if sation will change up or down
     //the first condition checks if my station is within range
     handleStationChange(station, key) {
@@ -13,57 +17,85 @@ class HomeView extends React.Component{
             return
         } else {
             if (key === "up"){
-
+                
             } else {
                 //here would be the logic for setting the station
             }
         }
     }
-
+    
+    
+    
     render(){
         return(
-        <>
-            {/* <Container> */}
+            <>
+            <div style={{flexGrow: 1, backgroundColor: 'purple', positon: "sticky", top: -5}}>
+                <h1
+                style={{textAlign: 'center'}}
+                >AVID FM Radio</h1>
 
-            <CardHeader/>
 
-            <Grid
-            container
-            // direction="row"
-            // justifyContent="space-between"
-            // alignItems="center"
+            {/* <AppBar
+            position="static"
+            // style={{backgroundColor: 'red'}}
+            disableGutters= {false}
             >
-            <Grid 
-            item xs={6}
-            zeroMinWidth
+                <Toolbar
+                disableGutters= {true}
+                style={{backgroundColor: 'red', paddingTop: '10px', marginTop: '10px', position: 0}}
+                >
+                    <IconButton>
+                        <Menu/>
+                    </IconButton>
+                    <IconButton>
+                        <Typography>Yeet</Typography>
+                    </IconButton>
+                </Toolbar>
+                </AppBar> */}
+            </div>
+            <div
+            style={{
+                display: "flex",
+                flexDirection: 'row',
+                justifyContent: 'space-evenly'
+            }}
             >
-                <Box>
-                    <p>Hello World</p>
-                </Box>
-            </Grid>
-             <Grid 
-             zeroMinWidth
-             item xs={6}
-             >  <Slider
-             defaultValue={5}
-             valueLabelDisplay="on"
-             step={1}
-             marks
-             min={0}
-             max={10}
-             />
-            </Grid>
-            </Grid>
-            {/* </Container> */}
+
+                <Draggable>
+                    <div>
+
+                        <Card
+                        style={{maxWidth: 300, minHeight: '300px'}}
+                        >
+                                <p>hello people</p>
+                        </Card>
+                </div>
+                </Draggable>
+                <Draggable>
+                    <Card
+                    style={{maxWidth: 300, padding: '50px'}}
+                    >
+                    <Slider
+                        defaultValue={5}
+                        valueLabelDisplay="on"
+                        step={1}
+                        marks
+                        min={0}
+                        max={10}
+                        />
+                        <br/>
+                        <p>this is where the hz adjuster would be</p>
+                    </Card>
+                </Draggable>
+            </div>
          </>
         );
     }
 }
 
-
-
-
-
+const header = {
+    color: 'red',
+};
 
 
 
