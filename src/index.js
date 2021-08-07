595a3cfccc3b092533553735e255a807bae6f0ab
Alejandro Estrada <aestrada611@gmail.com>
2021-08-07 13:35:09 -0700

    did not add new ui just styled some more

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {Slider, Card, AppBar, Toolbar, Typography, IconButton} from '@material-ui/core';
+import {Slider, Card} from '@material-ui/core';
 import Draggable from 'react-draggable';
-import {Menu} from '@material-ui/icons';
+// import {Menu} from '@material-ui/icons';
 import {Knob} from 'react-rotary-knob';
 import * as skins from 'react-rotary-knob-skin-pack';
 
@@ -14,17 +14,33 @@ const knobStyle = {
     height: "150px"
 }
 
+function Header(props) {
+    return(
+        <div style={{flexGrow: 1, backgroundColor: '#7950C7'}}>
+                <h1
+                style={{textAlign: 'center'}}
+                >AVID FM Radio</h1>
+        </div>
+    )
+}
+
+function Footer(props) {
+    return(
+        <div>
+            <h1 style={{backgroundColor: "white", textAlign: 'center', width: '100%', position: 'absolute', height: "10vh"}}>Hello World</h1>
+            {/* the view height here is related to the one for the view height of the body div on line 63 */}
+        </div>
+    )
+}
+
 
 class HomeView extends React.Component{
-
     state= {
         value: 5
     }
-
     changeVolume(volume) {
         this.setState({value:volume})
     }
-    
     //think of better name for key but will handle if sation will change up or down
     //the first condition checks if my station is within range
     handleStationChange(station, key) {
@@ -44,55 +60,54 @@ class HomeView extends React.Component{
     render(){
         return(
             <>
-            <div style={{flexGrow: 1, backgroundColor: '#7950C7', top: -5}}>
-                <h1
-                style={{textAlign: 'center'}}
-                >AVID FM Radio</h1>
-            </div>
-            <div
-            style={{
-                display: "flex",
-                flexDirection: 'row',
-                justifyContent: 'space-evenly'
-            }}
-            >
+                <Header/>
+                <div
+                style={{
+                    display: "flex",
+                    flexDirection: 'row',
+                    justifyContent: 'space-evenly',
+                    minHeight: '80vh',
+                }}
+                >
+                    <Draggable>
+                        <div>
 
-                <Draggable>
-                    <div>
-
-                        <Card
-                        style={{maxWidth: 300, minHeight: '300px', borderColor: '#7950C7', borderWidth: '3px', backgroundColor: 'rgb(66, 66, 66)'}}
-                        variant='outlined'
-                        >
+                            <Card
+                            style={{maxWidth: 300, minHeight: '300px', borderColor: '#7950C7', borderWidth: '3px', backgroundColor: 'rgb(66, 66, 66)'}}
+                            variant='outlined'
+                            >
                                 <p>radio.station</p>
-                        </Card>
+                            </Card>
+                    </div>
+                    </Draggable>
+                    <Draggable>
+                        <div>
+                            <Card
+                            style={{maxWidth: 300, padding: '50px', borderColor: '#7950C7', borderWidth: '3px', backgroundColor: 'rgb(66, 66, 66)'}}
+                            variant='outlined'
+                            >
+                            <Slider
+                                defaultValue={5}
+                                valueLabelDisplay="on"
+                                step={1}
+                                marks
+                                min={0}
+                                max={10}
+                                />
+                                <br/>
+                                <Knob
+                                onChange={this.changeVolume.bind(this)}
+                                min={0}
+                                max={10}
+                                value={this.state.value}
+                                skin={skins.s12}
+                                style={knobStyle}
+                                />
+                            </Card>
+                        </div>
+                    </Draggable>
                 </div>
-                </Draggable>
-                <Draggable>
-                    <Card
-                    style={{maxWidth: 300, padding: '50px', borderColor: '#7950C7', borderWidth: '3px', backgroundColor: 'rgb(66, 66, 66)'}}
-                    variant='outlined'
-                    >
-                    <Slider
-                        defaultValue={5}
-                        valueLabelDisplay="on"
-                        step={1}
-                        marks
-                        min={0}
-                        max={10}
-                        />
-                        <br/>
-                        <Knob
-                        onChange={this.changeVolume.bind(this)}
-                        min={0}
-                        max={10}
-                        value={this.state.value}
-                        skin={skins.s12}
-                        style={knobStyle}
-                        />
-                    </Card>
-                </Draggable>
-            </div>
+            <Footer/>
          </>
         );
     }
